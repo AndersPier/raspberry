24aa1f846ae484c9e84f42e4f5f7ce6c5c936bdf
Anders Lehmann Pier <anders@pier.dk>
2025-04-20 21:33:47 +0200

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -4,6 +4,8 @@ sudo apt install -y docker.io
 sudo apt install -y screenfetch
 echo /usr/bin/screenfetch >> ~/.bashrc
 sudo apt install -y cockpit
+sudo cp 01.netcfg.yaml /etc/netplan/01.netcfg.yaml
+sudo chmod ug+rw /etc/netplan/01.netcfg.yaml
 sudo apt install -y btop
 sudo curl -sSL https://install.pi-hole.net | bash
 sudo pihole setpassword
