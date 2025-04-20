f615b03910f054fda6b57551e75c084908a4f13e
Anders Lehmann Pier <anders@pier.dk>
2025-04-20 21:03:18 +0200

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -2,6 +2,8 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y docker.io
 sudo apt install -y screenfetch
+echo /usr/bin/screenfetch >> ~/.bashrc
 sudo apt install -y cockpit
 sudo apt install -y btop
 sudo curl -sSL https://install.pi-hole.net | bash
+sudo pihole setpasswor
