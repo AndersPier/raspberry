c5f8cb5544a6e46eb28b7993941f0ca4409fdc69
Anders Lehmann Pier <anders@pier.dk>
2025-04-20 21:50:53 +0200

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -5,7 +5,7 @@ sudo apt install -y screenfetch
 echo /usr/bin/screenfetch >> ~/.bashrc
 sudo apt install -y cockpit
 sudo cp 01.netcfg.yaml /etc/netplan/01.netcfg.yaml
-sudo chmod ug+rw /etc/netplan/01.netcfg.yaml
+sudo chmod 600 /etc/netplan/01.netcfg.yaml
 sudo netplan apply
 sudo apt install -y btop
 sudo curl -sSL https://install.pi-hole.net | bash
