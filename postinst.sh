c3b331aa3376d665751f109b43f915530639c1f2
Anders Lehmann Pier <anders@pier.dk>
2025-04-20 19:41:29 +0200

    Create postinst.sh

---
+++ postinst.sh
@@ -0,0 +1,7 @@
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y docker.io
+sudo apt install -y screenfetch
+sudo apt install -y cockpit
+sudo apt install -y btop
+sudo curl -sSL https://install.pi-hole.net | bash
